8c38648dcabd685700a23fb36f5ea2dcc13be260
Nicolas Palacios <a.nicojpo@gmail.com>
2021-08-02 16:46:43 -0400

    Progreso 16:46

---
+++ app/src/main/java/com/example/apppropiaalarma/Menu.java
@@ -0,0 +1,32 @@
+package com.example.apppropiaalarma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+
+    public void Nueva(View v) {
+        Intent siguiente = new Intent(this, Nueva.class);
+        startActivity(siguiente);
+    }
+
+    public void Config(View v) {
+        Intent siguiente = new Intent(this, Configurar.class);
+        startActivity(siguiente);
+    }
+    public void Historial(View v) {
+        Intent siguiente = new Intent(this, Historial.class);
+        startActivity(siguiente);
+    }
+
+}
